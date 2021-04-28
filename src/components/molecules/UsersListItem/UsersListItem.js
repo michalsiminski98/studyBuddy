1977ed21e6383a5d8303eb3b4c18d4bf6f968277
Michal Siminski <michal.siminski3@gmail.com>
2021-04-28 09:17:57 +0200

    events

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -2,14 +2,19 @@ import PropTypes from 'prop-types';
 import { Wrapper, StyledAverage, StyledInfo } from './UsersListItem.styles';
 import Button from 'components/atoms/Button/Button';
 
-const UsersListItem = ({ userData: { name, attendance = '0%', average } }) => {
+const UsersListItem = ({
+  userData: { id, name, attendance = '0%', average },
+}) => {
   return (
     <Wrapper>
       <StyledAverage value={average}>{average}</StyledAverage>
       <StyledInfo>
         <p>
           {name}
-          <Button />
+          <Button
+            studentId={id}
+            onMouseEnter={() => alert(`This is student #${id}`)}
+          />
         </p>
         <p>{attendance}</p>
       </StyledInfo>
